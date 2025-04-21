44ddf48c88aff4820387def6208828f1016830be
valentine ezugu <valentineezugu@yahoo.com>
2025-04-21 12:07:35 +0200

    saga repo

--- orderService/src/main/java/com/saga/orderService/repository/SagaStepRepository.java
+++ orderService/src/main/java/com/saga/orderService/repository/SagaStepRepository.java
@@ -2,5 +2,7 @@ package com.saga.orderService.repository;
 
 import com.saga.orderService.model.SagaStep;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SagaStepRepository extends JpaRepository<SagaStep, Long> {}
